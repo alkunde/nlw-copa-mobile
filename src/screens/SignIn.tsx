62ea4f6f4f6012af15fcd035135e6c991d4690ff
André Kunde <andre.kunde@portoseguro.com.br>
2023-09-19 14:52:26 -0300

    init svg transformer

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,11 +1,14 @@
 import { Center, Text } from 'native-base';
 
+import Logo from '../assets/logo.svg';
+import { Button } from '../components/Button';
+
 export function SignIn() {
   return (
     <Center flex={1} bg="gray.900">
-      <Text color="white" fontSize={24} fontFamily="heading">
-        SignIn
-      </Text>
+      <Logo width={212} height={40} />
+
+      <Button title='Entrar com google' />
     </Center>
   )
 }
